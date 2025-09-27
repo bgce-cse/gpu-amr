77c27f9d64f17d9330a8f157b5141874cb6131a5
MiguelVeganzones <veganzones28@gmail.com>
2025-09-27 22:51:50 +0200

    Add patch indexer 2D example

--- examples/patch_index.e.cpp
+++ examples/patch_index.e.cpp
@@ -1,1 +1,36 @@
-#include "containers/static_matrix.hpp"
+#include "containers/static_tensor.hpp"
+#include <iostream>
+
+int main()
+{
+    constexpr auto N = 6;
+    constexpr auto M = 8;
+    [[maybe_unused]]
+    constexpr auto Fanout = 2;
+    using tensor_t        = amr::containers::static_tensor<int, N, M>;
+
+    tensor_t from{};
+    std::ranges::iota(from, 0);
+    std::cout << from << '\n';
+    for (auto const& e : from)
+        std::cout << e << " ";
+    std::cout << '\n';
+
+    tensor_t to[Fanout * Fanout]{};
+    for (int i = 0; i != N; ++i)
+    {
+        for (int j = 0; j != M; ++j)
+        {
+            for (int ki = 0; ki != Fanout; ++ki)
+            {
+                for (int kj = 0; kj != Fanout; ++kj)
+                {
+                    to[ki * Fanout + kj][i, j] =
+                        (i / Fanout) * M + (j / Fanout) + M / Fanout * (ki * N + kj);
+                }
+            }
+        }
+    }
+    for (auto const& e : to)
+        std::cout << e << '\n';
+}
