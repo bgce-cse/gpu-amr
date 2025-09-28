77baaa18c589fe88615b605e8aacbbe9e445363b
MiguelVeganzones <veganzones28@gmail.com>
2025-09-28 19:55:57 +0200

    Add constexpr computation

--- examples/layout_patch_index.e.cpp
+++ examples/layout_patch_index.e.cpp
@@ -26,44 +26,45 @@ int main()
 
     // -------------------------------------
 
-    patch_shape_t to{};
-
-    auto idx           = typename tensor_t::multi_index_t{};
-    auto sized_strides = decltype(tensor_t::s_strides){};
-    std::transform(
-        std::cbegin(tensor_t::s_strides),
-        std::cend(tensor_t::s_strides),
-        std::cbegin(tensor_t::s_sizes),
-        std::begin(sized_strides),
-        std::multiplies{}
-    );
-    std::cout << '\n';
-    do
+    constexpr auto patch_index_to = []() constexpr
     {
-        const auto offset = std::transform_reduce(
-            std::cbegin(idx),
-            std::cend(idx),
+        patch_shape_t to{};
+
+        auto idx           = typename tensor_t::multi_index_t{};
+        auto sized_strides = decltype(tensor_t::s_strides){};
+        std::transform(
             std::cbegin(tensor_t::s_strides),
-            index_t{},
-            std::plus{},
-            [](index_t const i, index_t const s) { return (i / Fanout) * s; }
+            std::cend(tensor_t::s_strides),
+            std::cbegin(tensor_t::s_sizes),
+            std::begin(sized_strides),
+            std::multiplies{}
         );
-        auto out_patch_idx = typename patch_shape_t::multi_index_t{};
         do
         {
-            const auto base = std::transform_reduce(
-                                  std::cbegin(out_patch_idx),
-                                  std::cend(out_patch_idx),
-                                  std::cbegin(sized_strides),
-                                  index_t{}
-                              ) /
-                              Fanout;
-            to[out_patch_idx][idx] = offset + base;
-        } while (out_patch_idx.increment());
-    } while (idx.increment());
-
+            const auto offset = std::transform_reduce(
+                std::cbegin(idx),
+                std::cend(idx),
+                std::cbegin(tensor_t::s_strides),
+                index_t{},
+                std::plus{},
+                [](index_t const i, index_t const s) { return (i / Fanout) * s; }
+            );
+            auto out_patch_idx = typename patch_shape_t::multi_index_t{};
+            do
+            {
+                const auto base = std::transform_reduce(
+                                      std::cbegin(out_patch_idx),
+                                      std::cend(out_patch_idx),
+                                      std::cbegin(sized_strides),
+                                      index_t{}
+                                  ) /
+                                  Fanout;
+                to[out_patch_idx][idx] = offset + base;
+            } while (out_patch_idx.increment());
+        } while (idx.increment());
+        return to;
+    }();
     // -------------------------------------
 
-    for (auto const& e : to)
-        std::cout << e << '\n';
+    std::cout << "Patch index:\n" << patch_index_to << '\n';
 }
