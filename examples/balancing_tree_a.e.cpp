df31693f313026b95aca2176e7308a229763b42a
jschupp <j.schupp@tum.de>
2025-10-07 16:48:50 +0200

    adapt examples to recent changes

--- examples/balancing_tree_a.e.cpp
+++ examples/balancing_tree_a.e.cpp
@@ -68,19 +68,20 @@ auto operator<<(std::ostream& os, cell const& c) -> std::ostream&
 int main()
 {
     std::cout << "Hello balancing world\n";
-    constexpr auto N = 2;
-    constexpr auto M = 4;
+    constexpr size_t N = 4;
+    constexpr size_t M = 4;
+    constexpr size_t Halo = 1;
     // using linear_index_t    = std::uint32_t;
     [[maybe_unused]]
     constexpr auto Fanout = 2;
-    using tensor_t        = amr::containers::static_tensor<float, N, M>;
+    using tensor_t        = amr::containers::static_tensor<float, Halo,  N, M>;
 
     using patch_index_t       = amr::ndt::morton::morton_id<7u, 2u>;
     using tree_t        = amr::ndt::tree::ndtree<cell, patch_index_t, tensor_t>;
 
     tree_t h(100000000); // Provide initial capacity
 
-    ndt::print::example_patch_print<M,N> printer("debug_tree");
+    ndt::print::example_patch_print<Halo , M ,N> printer("debug_tree");
  
     auto refine_criterion = [](const patch_index_t& idx)
     {
@@ -129,12 +130,24 @@ int main()
         // Access S1 values (float)
         tensor_t& s1_patch = h.template get_patch<S1>(idx);
 
-        for(int linear_idx = 0; linear_idx < N*M; linear_idx++) {
-        s1_patch[linear_idx] = static_cast<float>(linear_idx);
+        for(size_t linear_idx = 0; linear_idx < N*M; linear_idx++) {
+            auto padded_index = tensor_t::layout_t::logical_to_full_index(linear_idx);
+            s1_patch[padded_index] = static_cast<float>(linear_idx);
         }
 
     }
+    for(size_t idx = 0; idx < h.size(); idx++){
+        // Access S1 values (float)
+        std::cout << "patch " << idx << std::endl;
+        tensor_t& s1_patch = h.template get_patch<S1>(idx);
 
+        for(size_t linear_idx = 0; linear_idx < 36; linear_idx++) {
+
+            std::cout << s1_patch[linear_idx] << std::endl; 
+        }
+
+    }
+// 
     printer.print(h, "_iteration_0.vtk");
 
     int i = 1;
@@ -144,6 +157,19 @@ int main()
         std::string file_extension = "_iteration_" + std::to_string(i) + ".vtk";
         printer.print(h, file_extension);
     }
+
+    // for(size_t idx = 0; idx < h.size(); idx++){
+    //     // Access S1 values (float)
+    //     std::cout << "patch " << idx << std::endl;
+    //     tensor_t& s1_patch = h.template get_patch<S1>(idx);
+
+    //     for(size_t linear_idx = 0; linear_idx < 36; linear_idx++) {
+
+    //         std::cout << s1_patch[linear_idx] << std::endl; 
+    //     }
+
+    // }
+
 
     for (; i != 13; ++i)
     {
