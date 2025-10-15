dcb4f1bfeb7f297a7a02e42b3f009ebb3cb75378
jschupp <j.schupp@tum.de>
2025-10-15 23:31:59 +0200

    fully implement neighbor stuff (finally working)

--- examples/balancing_tree_a.e.cpp
+++ examples/balancing_tree_a.e.cpp
@@ -72,8 +72,8 @@ int main()
 {
     std::cout << "Hello balancing world\n";
     constexpr std::size_t N    = 4;
-    constexpr std::size_t M    = 2;
-    constexpr std::size_t Halo = 2;
+    constexpr std::size_t M    = 4;
+    constexpr std::size_t Halo = 1;
     // using linear_index_t    = std::uint32_t;
     [[maybe_unused]]
     constexpr auto Fanout = 2;
@@ -81,11 +81,11 @@ int main()
     using layout_t        = amr::containers::static_layout<shape_t>;
     using index_t         = typename layout_t::index_t;
 
-    using patch_index_t  = amr::ndt::morton::morton_id<7u, 2u>;
+    using patch_index_t  = amr::ndt::morton::morton_id<9u, 2u>;
     using patch_layout_t = amr::ndt::patches::patch_layout<layout_t, Halo>;
     using tree_t         = amr::ndt::tree::ndtree<cell, patch_index_t, patch_layout_t>;
 
-    tree_t tree(100000000); // Provide initial capacity
+    tree_t tree(100000); // Provide initial capacity
 
     ndt::print::example_patch_print<Halo, M, N> printer("debug_tree");
 
@@ -110,25 +110,25 @@ int main()
         return tree_t::refine_status_t::Stable;
     };
 
-    // auto coarsen_criterion = [](const patch_index_t& idx)
-    // {
-    //     auto [coords, level] = patch_index_t::decode(idx.id());
-    //     auto max_size        = 1u << idx.max_depth();
-    //     auto cell_size       = 1u << (idx.max_depth() - level);
+    auto coarsen_criterion = [](const patch_index_t& idx)
+    {
+        auto [coords, level] = patch_index_t::decode(idx.id());
+        auto max_size        = 1u << idx.max_depth();
+        auto cell_size       = 1u << (idx.max_depth() - level);
 
-    //     double mid_x  = coords[0] + 0.5 * cell_size;
-    //     double mid_y  = coords[1] + 0.5 * cell_size;
-    //     double center = 0.5 * max_size;
-    //     double dist2 =
-    //         (mid_x - center) * (mid_x - center) + (mid_y - center) * (mid_y - center);
+        double mid_x  = coords[0] + 0.5 * cell_size;
+        double mid_y  = coords[1] + 0.5 * cell_size;
+        double center = 0.5 * max_size;
+        double dist2 =
+            (mid_x - center) * (mid_x - center) + (mid_y - center) * (mid_y - center);
 
-    //     // Only coarsen if not at min level!
-    //     if (level > 0 && dist2 < 0.3 / idx.level() * max_size * max_size)
-    //     {
-    //         return tree_t::refine_status_t::Coarsen;
-    //     }
-    //     return tree_t::refine_status_t::Stable;
-    // };
+        // Only coarsen if not at min level!
+        if (level > 0 && dist2 < 0.3 / idx.level() * max_size * max_size)
+        {
+            return tree_t::refine_status_t::Coarsen;
+        }
+        return tree_t::refine_status_t::Stable;
+    };
 
     int ii = 0;
     for (std::size_t idx = 0; idx < tree.size(); idx++)
@@ -146,46 +146,23 @@ int main()
             s1_patch[linear_idx] = static_cast<float>(ii++);
         }
     }
-    // for(size_t idx = 0; idx < tree.size(); idx++){
-    //     // Access S1 values (float)
-    //     std::cout << "patch " << idx << std::endl;
-    //     auto& s1_patch = tree.template get_patch<S1>(idx);
-
-    //     for(size_t linear_idx = 0; linear_idx < 36; linear_idx++) {
-
-    //         std::cout << s1_patch[linear_idx] << std::endl;
-    //     }
-
-    // }
     
     printer.print(tree, "_iteration_0.vtk");
 
     int i = 1;
-    for (; i != 3; ++i)
+    for (; i != 8; ++i)
     {
         tree.reconstruct_tree(refine_criterion);
         std::string file_extension = "_iteration_" + std::to_string(i) + ".vtk";
         printer.print(tree, file_extension);
     }
 
-    // for(size_t idx = 0; idx < tree.size(); idx++){
-    //     // Access S1 values (float)
-    //     std::cout << "patch " << idx << std::endl;
-    //     auto& s1_patch = tree.template get_patch<S1>(idx);
-
-    //     for(size_t linear_idx = 0; linear_idx < 36; linear_idx++) {
-
-    //         std::cout << s1_patch[linear_idx] << std::endl;
-    //     }
-
-    // }
-
-    // for (; i != 11; ++i)
-    // {
-    //     tree.reconstruct_tree(coarsen_criterion);
-    //     std::string file_extension = "_iteration_" + std::to_string(i) + ".vtk";
-    //     printer.print(tree, file_extension);
-    // }
+    for (; i != 15; ++i)
+    {
+        tree.reconstruct_tree(coarsen_criterion);
+        std::string file_extension = "_iteration_" + std::to_string(i) + ".vtk";
+        printer.print(tree, file_extension);
+    }
 
     std::cout << "adios balancing world\n";
     return EXIT_SUCCESS;
